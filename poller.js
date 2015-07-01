76faf5b7cf0c4a28ca7b74c3de09bcd5c800e6bf
Rich Parrish <richparrishx@gmail.com>
2015-07-01 15:07:52 -0700

    natural sort

--- poller.js
+++ poller.js
@@ -117,7 +117,7 @@ function generateStorageArray(stored){
     obj[key] = stored.Outbound[key];
     outbound.push(obj);
   }
-  return {Inbound: inbound.sort(natcmp), Outbound: outbound.sort(natcmp)};
+  return {Inbound: inbound.sort(naturalSort), Outbound: outbound.sort(naturalSort)};
 }
 
   // $.ajax({
@@ -200,29 +200,46 @@ function generateStorageArray(stored){
 
 module.exports = poller;
 
-// https://stackoverflow.com/questions/15478954/sort-array-elements-string-with-numbers-natural-sort
-function strcmp(a, b) {
-    return a > b ? 1 : a < b ? -1 : 0;
-}
-
-function natcmp(a, b) {
-    a = $('.busnumber', a).text()
-    b = $('.busnumber', b).text()
-    var x = [], y = [];
-
-    a.replace(/(\d+)|(\D+)/g, function($0, $1, $2) { x.push([$1 || 0, $2]) })
-    b.replace(/(\d+)|(\D+)/g, function($0, $1, $2) { y.push([$1 || 0, $2]) })
-
-    while(x.length && y.length) {
-        var xx = x.shift();
-        var yy = y.shift();
-        var nn = (xx[0] - yy[0]) || strcmp(xx[1], yy[1]);
-        if(nn) return nn;
+/*
+ * Natural Sort algorithm for Javascript - Version 0.7 - Released under MIT license
+ * Author: Jim Palmer (based on chunking idea from Dave Koelle)
+ */
+ function naturalSort (a, b) {
+    var re = /(^-?[0-9]+(\.?[0-9]*)[df]?e?[0-9]?$|^0x[0-9a-f]+$|[0-9]+)/gi,
+        sre = /(^[ ]*|[ ]*$)/g,
+        dre = /(^([\w ]+,?[\w ]+)?[\w ]+,?[\w ]+\d+:\d+(:\d+)?[\w ]?|^\d{1,4}[\/\-]\d{1,4}[\/\-]\d{1,4}|^\w+, \w+ \d+, \d{4})/,
+        hre = /^0x[0-9a-f]+$/i,
+        ore = /^0/,
+        i = function(s) { return naturalSort.insensitive && (''+s).toLowerCase() || ''+s },
+        // convert all to strings strip whitespace
+        x = i(a).replace(sre, '') || '',
+        y = i(b).replace(sre, '') || '',
+        // chunk/tokenize
+        xN = x.replace(re, '\0$1\0').replace(/\0$/,'').replace(/^\0/,'').split('\0'),
+        yN = y.replace(re, '\0$1\0').replace(/\0$/,'').replace(/^\0/,'').split('\0'),
+        // numeric, hex or date detection
+        xD = parseInt(x.match(hre)) || (xN.length != 1 && x.match(dre) && Date.parse(x)),
+        yD = parseInt(y.match(hre)) || xD && y.match(dre) && Date.parse(y) || null,
+        oFxNcL, oFyNcL;
+    // first try and sort Hex codes or Dates
+    if (yD)
+        if ( xD < yD ) return -1;
+        else if ( xD > yD ) return 1;
+    // natural sorting through split numeric strings and default strings
+    for(var cLoc=0, numS=Math.max(xN.length, yN.length); cLoc < numS; cLoc++) {
+        // find floats not starting with '0', string or 0 if not defined (Clint Priest)
+        oFxNcL = !(xN[cLoc] || '').match(ore) && parseFloat(xN[cLoc]) || xN[cLoc] || 0;
+        oFyNcL = !(yN[cLoc] || '').match(ore) && parseFloat(yN[cLoc]) || yN[cLoc] || 0;
+        // handle numeric vs string comparison - number < string - (Kyle Adams)
+        if (isNaN(oFxNcL) !== isNaN(oFyNcL)) { return (isNaN(oFxNcL)) ? 1 : -1; }
+        // rely on string comparison if different types - i.e. '02' < 2 != '02' < '2'
+        else if (typeof oFxNcL !== typeof oFyNcL) {
+            oFxNcL += '';
+            oFyNcL += '';
+        }
+        if (oFxNcL < oFyNcL) return -1;
+        if (oFxNcL > oFyNcL) return 1;
     }
-    // 5L follows 5
-    if(x.length) return +1;
-    if(y.length) return -1;
-
     return 0;
 }
 
